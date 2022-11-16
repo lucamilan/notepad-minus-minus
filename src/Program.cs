6d641707e0f31089ca7403952b3a89053786ac47
Luca Milan <luca_milan@hotmail.com>
2022-11-16 22:11:57 +0100

    appsettings

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,12 @@
-using System.Runtime.InteropServices;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
-using Microsoft.JSInterop;
 using MudBlazor;
 using MudBlazor.Services;
 using notepad;
 using notepad.Data;
+using notepad.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -23,10 +22,11 @@ builder.Services.AddMudServices(config =>
     config.SnackbarConfiguration.ShowTransitionDuration = 500;
     config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
 });
+builder.Services.AddSingleton<MarkdownService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddDbContextFactory<NotepadDbContext>(options =>
   options.UseSqlite($"Filename={DbConstants.SqliteDbFilename}")
-         .LogTo(Console.WriteLine,  new[] { CoreEventId.ContextDisposed, CoreEventId.ContextInitialized })
+         .LogTo(Console.WriteLine, new[] { CoreEventId.ContextDisposed, CoreEventId.ContextInitialized })
          //.EnableSensitiveDataLogging()
          );
 builder.Services.AddScoped<DataSynchronizer>();
---
+++ src/Settings.cs
@@ -0,0 +1,6 @@
+namespace notepad;
+
+public class Settings
+{
+    public string Title { get; set; } = null!;
+}
