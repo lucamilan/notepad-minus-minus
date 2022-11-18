6880ba9c4dc561473ed2b76560d7bd3b05604164
Luca Milan <luca.milan@selecta.it>
2022-11-18 05:32:03 +0100

    aot support

--- src/Data/DataSynchronizer.cs
+++ src/Data/DataSynchronizer.cs
@@ -7,15 +7,16 @@ namespace notepad.Data;
 public class DataSynchronizer
 {
     private readonly Task _firstTimeSetupTask;
-
+    private readonly IJSRuntime _jsRuntime;
     private readonly IDbContextFactory<NotepadDbContext> _dbContextFactory;
     private readonly ILogger<DataSynchronizer> _logger;
 
-    public DataSynchronizer(IJSRuntime js, IDbContextFactory<NotepadDbContext> dbContextFactory, ILogger<DataSynchronizer> logger)
+    public DataSynchronizer(IJSRuntime jsRuntime, IDbContextFactory<NotepadDbContext> dbContextFactory, ILogger<DataSynchronizer> logger)
     {
+        _jsRuntime = jsRuntime;
         _dbContextFactory = dbContextFactory;
         _logger = logger;
-        _firstTimeSetupTask = FirstTimeSetupAsync(js);
+        _firstTimeSetupTask = FirstTimeSetupAsync();
     }
 
     public async Task<NotepadDbContext> GetPreparedDbContextAsync()
@@ -24,18 +25,19 @@ public class DataSynchronizer
         return await _dbContextFactory.CreateDbContextAsync();
     }
 
-    private async Task FirstTimeSetupAsync(IJSRuntime js)
+    private async Task FirstTimeSetupAsync()
     {
-        var module = await js.InvokeAsync<IJSObjectReference>("import", "./dbstorage.js");
-
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Create("browser")))
         {
-            await module.InvokeVoidAsync("synchronizeFileWithIndexedDb", DbConstants.SqliteDbFilename);
+            var module = await _jsRuntime.InvokeAsync<IJSObjectReference>("import", "./dbstorage.js");
+            await module.InvokeVoidAsync("synchronizeFileWithIndexedDb", DbConstants.SqliteDbFilename);            
+            await module.DisposeAsync();
+            _logger.LogWarning($"Successfully disposed dbstorage.js");
         }
 
         await using var db = await _dbContextFactory.CreateDbContextAsync();
         await db.Database.EnsureCreatedAsync();
- 
-         _logger.LogWarning($"Successfully syncronized SQLite database file '{DbConstants.SqliteDbFilename}' in browser");
+
+        _logger.LogWarning($"Successfully syncronized SQLite database file '{DbConstants.SqliteDbFilename}' in browser");
     }
 }
